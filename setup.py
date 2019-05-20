a9ca18ab0138f67531e6839d25cca32ca903bc61
shivam301296 <shivam301296@gmail.com>
2019-05-20 08:04:41 +0530

    updated"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fancybox",
-    version="0.0.1",
+    version="0.1.1",
     author="Shivam Agrawal",
     author_email="shivam301296@gmail.com",
     description="Print beautiful message boxes on command line",
