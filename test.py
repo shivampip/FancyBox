f57bf397762af117951febbd459635141fef3c18
shivam301296 <shivam301296@gmail.com>
2019-05-20 07:45:54 +0530

    updated

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import fancybox as fb
 
 #https://unicode-table.com/en/#miscellaneous-symbols-and-pictographs
 
-fb.success("TEST SUCCESS")
-fb.error("ileNotFoundException")
-fb.info("This is Fancy Message Box")
-fb.warning("This works only with Python currently")
+fb.success("Test Successful")
+fb.error("IOError occured")
+fb.info("Setup Completed")
+fb.warning("name is NULL")
 
